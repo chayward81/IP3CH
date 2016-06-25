c7683131e5a610315911d4310bb2d9f86a39bb3c
Charlie Hayward <c.hayward.mail@gmail.com>
2016-06-25 00:38:48 -0700

    Add additional js backend logic to pass second spec of project specs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
+var pingPong = function(numbers) {
+  for (var i = 1; i <= numbers.length; i += 1) {
+  	if (numbers[i] % 3 === 0) {
+  		numbers.splice(i, 1, "ping");
+    }
+  }
+  return numbers;
+}
+
 var buildArray = function(numInput) {
   var array = [];
   for (var i = 1; i <= numInput; i+=1) {
 	   array.push(i);
   }
-  return array;
+  return array = pingPong(array);
 }
 
 $(document).ready(function() {
