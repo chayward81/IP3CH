3b01ddfae62729a0e1e197177e2ca293f3ef93e5
Charlie Hayward <c.hayward.mail@gmail.com>
2016-06-25 01:06:07 -0700

    Add additional backend logic to pass third spec of project, plus front end logic for display as list with additional styling to improve display

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var pingPong = function(numbers) {
   for (var i = 1; i <= numbers.length; i += 1) {
   	if (numbers[i] % 3 === 0) {
-  		numbers.splice(i, 1, "ping");
+  		numbers.splice(i, 1, "Ping");
+    } else if (numbers[i] % 5 === 0) {
+      numbers.splice(i, 1, "Pong");
     }
   }
   return numbers;
@@ -19,7 +21,11 @@ $(document).ready(function() {
   $("form#numbers").submit(function(event) {
     var numInput = parseInt($("input#numInput").val());
     var list = buildArray(numInput);
-    $("#output").text(list);
+    /*$("#output").text(list);*/
+    var newHTML = $.map(list, function(value) {
+      return('<li>' + value + '</li>');
+    });
+    $("#output").html(newHTML.join(""));
     event.preventDefault();
   });
 
